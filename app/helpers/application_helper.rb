a9ee80cdd443965bee2a01e80e5f302f3772dbe4
H. Miyano <hmiyano@me.com>
2019-03-26 02:13:14 +0900

    add rpizza

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,6 @@ module ApplicationHelper
     $all_grades = %i(pg1ac pg1ak pg1bc pg1bk pg2ac pg2ak pg2bc pg2bk pg2cc pg2ck pg3a pg3b pg3c g1a g1b g1c g1d g2a g2b g2c g2d)
     $all_wg = ["全て", "pg1ac", "pg1ak", "pg1bc", "pg1bk", "pg2ac", "pg2ak", "pg2bc", "pg2bk", "pg2cc", "pg2ck", "pg3a", "pg3b", "pg3c", "g1a", "g1b", "g1c", "g1d", "g2a", "g2b", "g2c", "g2d"]
     $wg = ["PG1","PG2", "PG2","PG3","G1A","G1","G2","G3"]
-    $all_restaurants = ["CR麻布十番店","CR恵比寿店","CR六本木ヒルズ店","CR代官山店","CR広尾店","CRアークヒルズ店","CR青山店","CR丸の内店","ファーム"]
+    $all_restaurants = ["CR麻布十番店","CR恵比寿店","CR六本木ヒルズ店","CR代官山店","CR広尾店","CRアークヒルズ店","CR青山店","CR丸の内店","RPIZZA六本木ヒルズ店","ファーム"]
     $all_chapters = ["全て","イニシャルチェック","ハウスルール","チョッパー","キッチン","共通","衛生管理","セーフティ管理","教育","採用","シフト","発注","棚卸","経営","クリスプウェイ","成長する","行動する","チームで働く","責任を持つ"]
 end
