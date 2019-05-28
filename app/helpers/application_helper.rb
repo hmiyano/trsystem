1d381edc8b6cefaabbb06105168909d957b627fe
H. Miyano <hmiyano@me.com>
2019-05-28 13:36:07 +0900

    for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,6 @@ module ApplicationHelper
     $all_grades = %i(pg1ac pg1ak pg1bc pg1bk pg2ac pg2ak pg2bc pg2bk pg2cc pg2ck pg3a pg3b pg3c g1a g1b g1c g1d g2a g2b g2c g2d rp_pg1ac rp_pg1ak)
     $all_wg = ["全て", "pg1ac", "pg1ak", "pg1bc", "pg1bk", "pg2ac", "pg2ak", "pg2bc", "pg2bk", "pg2cc", "pg2ck", "pg3a", "pg3b", "pg3c", "g1a", "g1b", "g1c", "g1d", "g2a", "g2b", "g2c", "g2d"]
     $wg = ["PG1","PG2", "PG2","PG3","G1A","G1","G2","G3"]
-    $all_restaurants = ["CR麻布十番店","CR恵比寿店","CR六本木ヒルズ店","CR代官山店","CR広尾店","CRアークヒルズ店","CR青山店","CR丸の内店","RPIZZA六本木ヒルズ店","駒沢公園店","新宿南口店","COREDO室町店","渋谷スクランブルスクエア店","南町田グランベリーパーク店","ファーム"]
+    $all_restaurants = ["CR麻布十番店","CR恵比寿店","CR六本木ヒルズ店","CR代官山店","CR広尾店","CRアークヒルズ店","CR青山店","CR丸の内店","RPIZZA六本木ヒルズ店","駒沢公園店","ファーム"]
     $all_chapters = ["全て","イニシャルチェック","ハウスルール","チョッパー","キッチン","共通","衛生管理","セーフティ管理","教育","採用","シフト","発注","棚卸","経営","クリスプウェイ","成長する","行動する","チームで働く","責任を持つ"]
 end
